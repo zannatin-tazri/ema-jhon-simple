0c7ed8c5d7b675404a379d36cf31f4f40f2f3165
zannatin-tazri <zannatintazrizan@gmail.com>
2023-04-03 17:26:51 +0600

    retrived product from stored cart

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -16,8 +16,20 @@ const Shop = () => {
 
     useEffect(()=>{
         const storedCart=getShoppingCart();
-        console.log(storedCart)
-    },[])
+        const savedCart=[];
+        for(const id in storedCart){
+            // console.log(id);
+            const addedProduct=products.find(product=>product.id
+                ===id);
+                if(addedProduct){
+                     // get quantity of the products
+                const quantity=storedCart[id];
+                addedProduct.quantity=quantity;
+                savedCart.push(addedProduct);
+                }  
+        }
+        setCart(savedCart);
+    },[products])
 
     const handleAddToCart=(product)=>{
         // cart.push
